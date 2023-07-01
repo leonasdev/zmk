7cb96c8d63dc341bf6b1be2a2fc3759d4dcb644f
Aakash Dabas <entdabas@gmail.com>
2023-07-01 21:39:25 +0530

    testing

--- app/boards/shields/nice_view/widgets/peripheral_status.c
+++ app/boards/shields/nice_view/widgets/peripheral_status.c
@@ -66,15 +66,15 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
     // lv_obj_set_size(widget->obj, 160, 68);
 
     LOG_DBG("Set source to cat roll images!");
-    // lv_anim_init(&widget->anim);
-    // lv_anim_set_var(&widget->anim, widget->obj);
-    // lv_anim_set_time(&widget->anim, 1000);
-    // lv_anim_set_values(&widget->anim, 0, 10);
-    // lv_anim_set_exec_cb(&widget->anim, (lv_anim_exec_xcb_t)set_img_src);
-    // lv_anim_set_repeat_count(&widget->anim, LV_ANIM_REPEAT_INFINITE);
-    // lv_anim_set_repeat_delay(&widget->anim, 100);
-    // images = cat_roll_images;
-    // lv_anim_start(&widget->anim);
+    lv_anim_init(&widget->anim);
+    lv_anim_set_var(&widget->anim, widget->obj);
+    lv_anim_set_time(&widget->anim, 1000);
+    lv_anim_set_values(&widget->anim, 0, 10);
+    lv_anim_set_exec_cb(&widget->anim, (lv_anim_exec_xcb_t)set_img_src);
+    lv_anim_set_repeat_count(&widget->anim, LV_ANIM_REPEAT_INFINITE);
+    lv_anim_set_repeat_delay(&widget->anim, 100);
+    images = cat_roll_images;
+    lv_anim_start(&widget->anim);
 
     sys_slist_append(&widgets, &widget->node);
 
