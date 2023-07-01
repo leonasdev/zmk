bf4de6b6e0f0f2ce8fb86e91554596fd961a74fd
Aakash Dabas <entdabas@gmail.com>
2023-07-01 21:30:40 +0530

    testing

--- app/boards/shields/nice_view/widgets/peripheral_status.c
+++ app/boards/shields/nice_view/widgets/peripheral_status.c
@@ -6,7 +6,6 @@
  */
 
 #include <zephyr/kernel.h>
-#include <zephyr/bluetooth/services/bas.h>
 #include <zephyr/random/rand32.h>
 
 #include <zephyr/logging/log.h>
@@ -57,104 +56,25 @@ const void *cat_roll_images[] = {
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
-struct peripheral_status_state {
-    bool connected;
-};
-
-static void draw_top(lv_obj_t *widget, lv_color_t cbuf[], struct status_state state) {
-    lv_obj_t *canvas = lv_obj_get_child(widget, 0);
-
-    lv_draw_label_dsc_t label_dsc;
-    init_label_dsc(&label_dsc, LVGL_FOREGROUND, &lv_font_montserrat_16, LV_TEXT_ALIGN_RIGHT);
-    lv_draw_rect_dsc_t rect_black_dsc;
-    init_rect_dsc(&rect_black_dsc, LVGL_BACKGROUND);
-
-    // Fill background
-    lv_canvas_draw_rect(canvas, 0, 0, DISP_WIDTH, BATTERY_HEIGHT, &rect_black_dsc);
-
-    // Draw battery
-    draw_battery(canvas, state);
-
-    // Draw output status
-    lv_canvas_draw_text(canvas, 0, 0, DISP_WIDTH, &label_dsc,
-                        state.connected ? LV_SYMBOL_WIFI : LV_SYMBOL_CLOSE);
-
-    // Rotate canvas
-    rotate_canvas(canvas, cbuf);
-}
-
-static void set_battery_status(struct zmk_widget_status *widget,
-                               struct battery_status_state state) {
-#if IS_ENABLED(CONFIG_USB_DEVICE_STACK)
-    widget->state.charging = state.usb_present;
-#endif /* IS_ENABLED(CONFIG_USB_DEVICE_STACK) */
-
-    widget->state.battery = state.level;
-
-    draw_top(widget->obj, widget->cbuf, widget->state);
-}
-
-static void battery_status_update_cb(struct battery_status_state state) {
-    struct zmk_widget_status *widget;
-    SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) { set_battery_status(widget, state); }
-}
-
-static struct battery_status_state battery_status_get_state(const zmk_event_t *eh) {
-    return (struct battery_status_state) {
-        .level = bt_bas_get_battery_level(),
-#if IS_ENABLED(CONFIG_USB_DEVICE_STACK)
-        .usb_present = zmk_usb_is_powered(),
-#endif /* IS_ENABLED(CONFIG_USB_DEVICE_STACK) */
-    };
-}
-
-ZMK_DISPLAY_WIDGET_LISTENER(widget_battery_status, struct battery_status_state,
-                            battery_status_update_cb, battery_status_get_state)
-
-ZMK_SUBSCRIPTION(widget_battery_status, zmk_battery_state_changed);
-#if IS_ENABLED(CONFIG_USB_DEVICE_STACK)
-ZMK_SUBSCRIPTION(widget_battery_status, zmk_usb_conn_state_changed);
-#endif /* IS_ENABLED(CONFIG_USB_DEVICE_STACK) */
-
-static struct peripheral_status_state get_state(const zmk_event_t *_eh) {
-    return (struct peripheral_status_state){.connected = zmk_split_bt_peripheral_is_connected()};
-}
-
-static void set_connection_status(struct zmk_widget_status *widget,
-                                  struct peripheral_status_state state) {
-    widget->state.connected = state.connected;
-
-    draw_top(widget->obj, widget->cbuf, widget->state);
-}
-
-static void output_status_update_cb(struct peripheral_status_state state) {
-    struct zmk_widget_status *widget;
-    SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) { set_connection_status(widget, state); }
-}
-
-ZMK_DISPLAY_WIDGET_LISTENER(widget_peripheral_status, struct peripheral_status_state,
-                            output_status_update_cb, get_state)
-ZMK_SUBSCRIPTION(widget_peripheral_status, zmk_split_peripheral_status_changed);
-
 void set_img_src(void *var, int32_t val) {
     lv_obj_t *img = (lv_obj_t *)var;
     lv_img_set_src(img, images[val]);
 }
 
 int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
-    widget->obj = lv_obj_create(parent);
-    lv_obj_set_size(widget->obj, 160, 68);
+    widget->obj = lv_img_create(parent);
+    // lv_obj_set_size(widget->obj, 160, 68);
 
     LOG_DBG("Set source to cat roll images!");
-    lv_anim_init(&widget->anim);
-    lv_anim_set_var(&widget->anim, widget->obj);
-    lv_anim_set_time(&widget->anim, 1000);
-    lv_anim_set_values(&widget->anim, 0, 23);
-    lv_anim_set_exec_cb(&widget->anim, set_img_src);
-    lv_anim_set_repeat_count(&widget->anim, 10);
-    lv_anim_set_repeat_delay(&widget->anim, 100);
-    images = cat_roll_images;
-    lv_anim_start(&widget->anim);
+    // lv_anim_init(&widget->anim);
+    // lv_anim_set_var(&widget->anim, widget->obj);
+    // lv_anim_set_time(&widget->anim, 1000);
+    // lv_anim_set_values(&widget->anim, 0, 10);
+    // lv_anim_set_exec_cb(&widget->anim, (lv_anim_exec_xcb_t)set_img_src);
+    // lv_anim_set_repeat_count(&widget->anim, LV_ANIM_REPEAT_INFINITE);
+    // lv_anim_set_repeat_delay(&widget->anim, 100);
+    // images = cat_roll_images;
+    // lv_anim_start(&widget->anim);
 
     sys_slist_append(&widgets, &widget->node);
 
