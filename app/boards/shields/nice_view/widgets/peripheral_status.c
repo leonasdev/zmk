4889e292ceca945cbd1bb2d2a2ed3cfa6f137742
Aakash Dabas <entdabas@gmail.com>
2023-07-03 00:58:16 +0530

    Adding battery/connection widgets #4.1

--- app/boards/shields/nice_view/widgets/peripheral_status.c
+++ app/boards/shields/nice_view/widgets/peripheral_status.c
@@ -436,7 +436,7 @@ static void draw_top(lv_obj_t *widget, lv_color_t cbuf[], struct status_state st
                         state.connected ? LV_SYMBOL_WIFI : LV_SYMBOL_CLOSE);
 
     // Rotate canvas
-    rotate_canvas(canvas, cbuf);
+    // rotate_canvas(canvas, cbuf);
 }
 
 static void set_battery_status(struct zmk_widget_status *widget,
